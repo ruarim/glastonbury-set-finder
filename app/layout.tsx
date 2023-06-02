36180a8623b25fdba5075e457b02dabf090491ad
RuariM <48099261+RuariM@users.noreply.github.com>
2023-06-02 22:54:43 +0100

    reduce padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} grid grid-cols-1  h-screen place-items-center px-4`}
+        className={`${inter.className} grid grid-cols-1  h-screen place-items-center px-3`}
       >
         {children}
       </body>
