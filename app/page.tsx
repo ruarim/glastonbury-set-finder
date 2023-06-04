f4716f44800539b3c2f7c7bb4944b94386763220
RuariM <48099261+RuariM@users.noreply.github.com>
2023-06-04 17:32:39 +0100

    fix landing page spacing

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import SpotifyLogo from "@/components/spotifyLogo";
 export default function Home() {
   return (
     <main className="space-y-3 md:w-[600px] ">
-      <div className="border border-gray-500 rounded-lg p-8 flex flex-col justify-between space-y-12">
+      <div className="border border-gray-500 rounded-lg p-8 flex flex-col justify-between space-y-6">
         <div className="space-y-3">
           <h1 className="font-bold justify-center text-5xl text-center pt-3">
             <div className="grid grid-cols-1 place-items-center p-2">
@@ -21,9 +21,7 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="pt">
-          <AuthSpotify />
-        </div>
+        <AuthSpotify />
       </div>
     </main>
   );
