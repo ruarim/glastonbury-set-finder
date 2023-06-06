cff87a68de52e1b0574ee749554ea7de0f9b34e4
RuariM <48099261+RuariM@users.noreply.github.com>
2023-06-06 15:20:48 +0100

    png logo logo

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const title = "Glasto Finder";
 const description =
   "Receive personalised Glastonbury performance recommendations based on your Spotify liked tracks.";
 const url = `${process.env.NEXT_PUBLIC_BASE_URL}`;
-const image = `${url}/glastoFinder.svg`;
+const image = `${url}/glasto-finder-logo.png`;
 
 export const metadata: Metadata = {
   title,
