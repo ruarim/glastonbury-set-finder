8dd07fa569c698d968e4857bb9329e86d9737c48
RuariM <48099261+RuariM@users.noreply.github.com>
2023-06-06 15:07:09 +0100

    use next metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,44 @@
 import AuthSpotify from "@/components/authSpotify";
-import MetaTags from "@/components/metaTags";
 import SpotifyLogo from "@/components/spotifyLogo";
+import { Metadata } from "next";
+
+const title = "Glasto Finder";
+const description =
+  "Receive personalised Glastonbury performance recommendations based on your Spotify liked tracks.";
+const url = `${process.env.NEXT_PUBLIC_BASE_URL}`;
+const image = "/glastoFinder.svg";
+
+export const metadata: Metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    url,
+    images: [
+      {
+        url: image,
+        width: 1200,
+        height: 630,
+        alt: "Glasto Finder",
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return (
     <main className="space-y-3 md:w-[600px] ">
-      <MetaTags
-        title="Glasto Finder"
-        description="Receive personalised Glastonbury performance recommendations based on your Spotify liked tracks."
-        url={`${process.env.NEXT_PUBLIC_BASE_URL}`}
-        image={"@/public/glastoFinder.svg"}
-      />
       <div className="border border-gray-500 rounded-lg p-8 flex flex-col justify-between space-y-6">
         <div className="space-y-3">
           <h1 className="font-bold justify-center text-5xl text-center pt-3">
             <div className="grid grid-cols-1 place-items-center p-2">
               <SpotifyLogo />
             </div>
-            Glasto Finder
+            {title}
           </h1>
 
-          <p className="text-center text-lg">
-            Receive personalised Glastonbury performance recommendations based
-            on your Spotify liked tracks.
-          </p>
+          <p className="text-center text-lg">{description}</p>
         </div>
 
         <AuthSpotify />
