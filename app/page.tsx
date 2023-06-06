46759e4cc6036b22bb7cf234982ed98310b902e7
RuariM <48099261+RuariM@users.noreply.github.com>
2023-06-06 14:14:17 +0100

    add link preview metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-
 import AuthSpotify from "@/components/authSpotify";
+import MetaTags from "@/components/metaTags";
 import SpotifyLogo from "@/components/spotifyLogo";
 
 export default function Home() {
   return (
     <main className="space-y-3 md:w-[600px] ">
+      <MetaTags
+        title="Glasto Finder"
+        description="Receive personalised Glastonbury performance recommendations based on your Spotify liked tracks."
+        url={`${process.env.NEXT_PUBLIC_BASE_URL}`}
+        image={"@/public/glastoFinder.png"}
+      />
       <div className="border border-gray-500 rounded-lg p-8 flex flex-col justify-between space-y-6">
         <div className="space-y-3">
           <h1 className="font-bold justify-center text-5xl text-center pt-3">
