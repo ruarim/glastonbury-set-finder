87c76203eb096f1eb0cbb0d69b1de27f2fbd0685
RuariM <48099261+RuariM@users.noreply.github.com>
2023-06-06 14:26:32 +0100

    update preview

--- components/metaTags.tsx
+++ components/metaTags.tsx
@@ -8,11 +8,9 @@ interface MetaTagsProps {
 }
 
 const MetaTags = ({ title, description, url, image }: MetaTagsProps) => {
-
   return (
-    <Head>
+    <Head key={"glasto-finder-preview-metadata"}>
       <title>{title}</title>
-      <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
