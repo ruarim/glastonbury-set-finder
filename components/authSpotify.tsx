e976342f06b81c26b8dd4f53f9e9a10b8f7413f0
RuariM <48099261+RuariM@users.noreply.github.com>
2023-06-08 18:28:22 +0100

    auth loading clean up

--- components/authSpotify.tsx
+++ components/authSpotify.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Button from "./button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const AuthSpotify = () => {
   const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI;
@@ -8,6 +8,10 @@ const AuthSpotify = () => {
   const responseType = "code";
   const spotifyAuthUrl = `https://accounts.spotify.com/en/authorize?response_type=${responseType}&client_id=${clientId}&scope=user-library-read&redirect_uri=${redirectUri}&show_dialog=false`;
   const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    return () => setLoading(false);
+  }, []);
 
   return (
     <a href={spotifyAuthUrl}>
