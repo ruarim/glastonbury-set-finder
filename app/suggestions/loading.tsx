70e6aee27daab170297b9af69f16175be3efe2be
RuariM <48099261+RuariM@users.noreply.github.com>
2023-08-08 00:46:50 +0100

    fix mobile loading

--- app/suggestions/loading.tsx
+++ app/suggestions/loading.tsx
@@ -1,12 +1,10 @@
-"use client";
-
-import { ScaleLoader } from "react-spinners";
+import Scale from "@/components/ui/loading/scale-loader";
 
 export default function SuggestionsLoading() {
   return (
     <div>
       <div className="flex justify-center pb-4">
-        <ScaleLoader loading={true} color="white" height={18} width={12} />
+        <Scale />
       </div>
       <span className="text-center font-bold">Analysing liked tracks...</span>
     </div>
---
+++ components/ui/loading/scale-loader.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { ScaleLoader } from "react-spinners";
+
+export default function Scale() {
+  return <ScaleLoader loading={true} color="white" height={18} width={12} />;
+}
