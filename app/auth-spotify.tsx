837526b849f6d1434393d9c58adf7e7070a5cdb0
RuariM <48099261+RuariM@users.noreply.github.com>
2023-08-07 10:29:15 +0100

    update build and spotify auth

--- app/auth-spotify.tsx
+++ app/auth-spotify.tsx
@@ -3,7 +3,7 @@
 import Button from "../components/ui/button";
 import Link from "next/link";
 
-const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI;
+const redirectUri = window.location.href + "/suggestions";
 const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
 const responseType = "code";
 const spotifyAuthUrl = `https://accounts.spotify.com/en/authorize?response_type=${responseType}&client_id=${clientId}&scope=user-library-read&redirect_uri=${redirectUri}&show_dialog=false`;
