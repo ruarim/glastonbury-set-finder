0f6e6fc540ee0659e1e0e4abdaf9cecda8760eb7
RuariM <48099261+RuariM@users.noreply.github.com>
2023-08-08 12:20:21 +0100

    try axios

--- app/suggestions/queries/get-suggestions.ts
+++ app/suggestions/queries/get-suggestions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import axios from "axios";
 import { SuggestionsResponse } from "../types";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -8,16 +9,16 @@ export const fetchSuggestions = async (code: string) => {
   const url = `${BASE_URL}/suggestions/api/suggest?code=${code}`;
 
   try {
-    const response = await fetch(url);
+    const response = await axios.get(url);
 
-    if (!response.ok) {
+    if (response.status != 200) {
       throw new Error(
         "Error fetching suggestions: Server responded with status " +
           response.status
       );
     }
 
-    const data = await response.json();
+    const data = await response.data();
     return data as SuggestionsResponse;
   } catch (error) {
     if (error instanceof Error) throw new Error("Error: " + error.message);
