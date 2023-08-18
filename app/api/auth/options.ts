ad8dccfdcf2cd55cfee703a9639075f535cc7eda
RuariM <48099261+RuariM@users.noreply.github.com>
2023-08-18 17:24:12 +0100

    remove google auth

--- app/api/auth/options.ts
+++ app/api/auth/options.ts
@@ -1,11 +1,8 @@
 import { AuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import GoogleProvider from "next-auth/providers/google";
 
 const spotifyClientId = `${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}`;
-const spotifySecret = `${process.env.NEXT_PRIVATE_SPOTIFY_SECRET_ID}`;
-const googleClientId = `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`;
-const googleSecret = `${process.env.NEXT_PRIVATE_GOOGLE_CLIENT_SECRET}`;
+const spotifySecret = `${process.env.NEXT_PRIVATE_SPOTIFY_CLIENT_SECRET}`;
 const secret = `${process.env.NEXTAUTH_SECRET}`;
 
 export const options: AuthOptions = {
@@ -13,10 +10,6 @@ export const options: AuthOptions = {
     SpotifyProvider({
       clientId: spotifyClientId,
       clientSecret: spotifySecret,
-    }),
-    GoogleProvider({
-      clientId: googleClientId,
-      clientSecret: googleSecret,
     }),
   ],
   session: {
--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { Transition } from "@headlessui/react";
 import Button from "../ui/button";
 import { signIn } from "next-auth/react";
-import { Fragment } from "react";
 import SpotifyLogo from "../ui/icons/spotify-logo";
-import GoogleLogo from "../ui/icons/google-logo";
 
 export default function Login() {
   return (
@@ -34,67 +31,8 @@ export default function Login() {
               <span>Spotify account</span> <SpotifyLogo />
             </Button>
           </div>
-          <div className=" sm:rounded-lg">
-            <Button
-              onClick={() => signIn("google")}
-              type="submit"
-              className="px-5 space-x-1"
-            >
-              <span>Google account</span> <GoogleLogo />
-            </Button>
-          </div>
         </div>
       </div>
     </div>
   );
 }
-
-interface ModalProps {
-  title?: string;
-  children: React.ReactNode;
-}
-
-const Modal = ({ title, children }: ModalProps) => {
-  return (
-    <Transition.Root show={true} as={Fragment}>
-      <div className="relative ">
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 backdrop-blur-lg transition-opacity z-20" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-20 overflow-y-auto ">
-          <div className="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <div className="border border-gray-500 rounded-lg">
-                <div className="relative transform rounded-lg bg-black px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                  {
-                    <div className="flex justify-between">
-                      <h3 className="text-xl font-medium leading-6">{title}</h3>
-                    </div>
-                  }
-                  {children}
-                </div>
-              </div>
-            </Transition.Child>
-          </div>
-        </div>
-      </div>
-    </Transition.Root>
-  );
-};
